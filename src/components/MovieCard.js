ee346c8d429699a667d0424e6f7e0ad560f101a9
Andrew Onyancha <mr.onyanchaandrew@gmail.com>
2022-09-28 11:22:13 +0300

    completed assignment

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+{ /*
 function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
@@ -10,6 +11,16 @@ function MovieCard({
       <img src={posterSrc} alt={title} />
       <h2>{title}</h2>
       <small>{genres.join(", ")}</small>
+    </div>
+  );
+}
+*/}
+function MovieCard(props) {
+  return (
+    <div className="movie-card">
+      <img src={props.posterSrc} alt={props.title} />
+      <h2>{props.title}</h2>
+      <small>{props.genres.join(", ")}</small>
     </div>
   );
 }
